58931e3510d167a3b88955ed062e90e8a591a8db
orbella <jadhavidhi06@gmail.com>
2026-03-18 02:24:06 +0530

    created

---
+++ IndianunchBox/authentication/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.db import connection
+import hashlib
+
+# HASH PASSWORD
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'base.html')
+
+
+# REGISTER
+def register_view(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        password = hash_password(request.POST['password'])
+
+        with connection.cursor() as cursor:
+            cursor.callproc('RegisterUser', [name, email, password])
+
+        return redirect('login')
+
+    return render(request, 'auth/register.html')
+
+
+# LOGIN
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = hash_password(request.POST['password'])
+
+        with connection.cursor() as cursor:
+            cursor.callproc('LoginUser', [email])
+            user = cursor.fetchone()
+
+        if user and user[3] == password:
+            request.session['user_id'] = user[0]
+            request.session['user_name'] = user[1]
+            return redirect('home')
+
+    return render(request, 'auth/login.html')
